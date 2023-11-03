222f98ccff9b7c64b59d085be1ca2c0a49185af2
Felix <felix.ahlcrona@axema.se>
2023-11-03 11:03:26 +0100

    blazor render

--- CheatSheet/CheatSheet.cs
+++ CheatSheet/CheatSheet.cs
@@ -27,7 +27,11 @@ namespace CheatSheet
             Console.Read();
         }
 
-
+        // Blazor serverside.
+        //När du kör async metoder kan du behöva köra "await InvokeAsync(StateHasChanged);" för att uppdatera ui statet. 
+        //    Men annars kan du leka runt med onrender/oninitalized beroende på när de ska renderas. 
+        //    Alla metoder som inte behöver vara async ska var icke async, annars kan de bli fel där med
+        //https://steven-giesel.com/blogPost/4fd1ba1d-f244-460e-8de4-06f2cc9c19cb
 
         public static void NameOfJsonContains()
         {
